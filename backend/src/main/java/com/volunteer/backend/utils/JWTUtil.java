2f95642acf4ec29c3476e3ac4dd2ef83e7d30990
Petrichor813 <1321180567@qq.com>
2026-02-22 09:26:02 +0800

    feat: 加入JJWT工具类

---
+++ backend/src/main/java/com/volunteer/backend/utils/JWTUtil.java
@@ -0,0 +1,69 @@
+package com.volunteer.backend.utils;
+
+import java.util.Base64;
+import java.util.Date;
+
+import javax.crypto.SecretKey;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+
+@Component
+public class JWTUtil {
+    private final long expiration; // 单位是毫秒
+    private final SecretKey key;
+
+    // @formatter:off
+    public JWTUtil(
+        @Value("${jwt.secret}") String secret,
+        @Value("${jwt.expiration}") long expiration
+    ) {
+        // @formatter:on
+        byte[] decodedKey = Base64.getUrlDecoder().decode(secret);
+        this.key = Keys.hmacShaKeyFor(decodedKey);
+        this.expiration = expiration;
+    }
+
+    Claims parseClaims(String token) {
+        // @formatter:off
+        return Jwts.parser()
+            .verifyWith(key)            // 使用密钥常量验证所有签名的 JWT
+            .build()                    // 创建并返回线程安全的 JWTParser 对象
+            .parseSignedClaims(token)   // 解析 token，验证签名，自动检查过期时间
+            .getPayload();              // 获取 Claims
+        // @formatter:on
+    }
+
+    public String generateToken(String username) {
+        Date now = new Date();
+        Date expiryDate = new Date(now.getTime() + expiration);
+
+        // @formatter:off
+        return Jwts.builder()
+            .subject(username)      // 存放用户标识
+            .issuedAt(now)          // 签发时间
+            .expiration(expiryDate) // 过期时间
+            .signWith(key)          // 用密钥签名
+            .compact();
+        // @formatter:on
+    }
+
+    public String getUsernameFromToken(String token) {
+        Claims claims = parseClaims(token);
+        return claims.getSubject();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            parseClaims(token);
+            return true;
+        } catch (JwtException | IllegalArgumentException e) {
+            return false;
+        }
+    }
+}
