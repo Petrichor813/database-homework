6bfc48fff74c945d6f6b12b7361d56d8d82f250a
Petrichor813 <1321180567@qq.com>
2026-03-08 19:35:46 +0800

    style: 去掉不必要图片扩展名的模块

--- frontend/src/shims-vue.d.ts
+++ frontend/src/shims-vue.d.ts
@@ -13,18 +13,3 @@ declare module "*.jpeg" {
   const src: string;
   export default src;
 }
-
-declare module "*.png" {
-  const src: string;
-  export default src;
-}
-
-declare module "*.gif" {
-  const src: string;
-  export default src;
-}
-
-declare module "*.svg" {
-  const src: string;
-  export default src;
-}
